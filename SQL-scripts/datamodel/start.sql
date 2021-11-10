a1623228042f948f68ab28824ff4e4868b37b7ed
kremenevskiy <kremeneuski@gmail.com>
2021-11-10 05:26:42 +0300

    fix: when delete set null for not nullable

--- SQL-scripts/datamodel/start.sql
+++ SQL-scripts/datamodel/start.sql
@@ -1,14 +1,14 @@
 USE master
-IF DB_ID (N'JournaForSchool') IS NOT NULL 
+IF DB_ID (N'JournalForSchool') IS NOT NULL 
 BEGIN
-  ALTER DATABASE JournaForSchool SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-  DROP DATABASE JournaForSchool;
+  ALTER DATABASE JournalForSchool SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE JournalForSchool;
 END;
 GO
 
-CREATE DATEBASE JournaForSchool
+CREATE DATABASE JournalForSchool;
 GO
-USE JournaForSchool
+USE JournalForSchool;
 GO
 
 
@@ -16,8 +16,15 @@ CREATE TABLE TheClasses(
     Id INT IDENTITY(1, 1) NOT NULL,
     TheClass INT NOT NULL,
     ClassLetter NCHAR(1) NOT NULL,
-    CONSTRAINT PK_TheClasses PRIMARY KEY CLUSTERED(Id ASC)
+    PRIMARY KEY CLUSTERED(Id ASC)
 );
+
+CREATE TABLE Subjects(
+    Id INT IDENTITY(1, 1) NOT NULL,
+    SubjectName NVARCHAR(50) UNIQUE NOT NULL,
+    CONSTRAINT PK_Subjects PRIMARY KEY CLUSTERED(Id ASC)
+);
+
 
 
 CREATE TABLE Users(
@@ -25,11 +32,9 @@ CREATE TABLE Users(
     FirstName NVARCHAR(30) NOT NULL,
     MiddleName NVARCHAR(30) NOT NULL,
     LastName NVARCHAR(30) NOT NULL,
-    TheClassesId INT NOT NULL,
+    TheClassesId INT NULL,
     Login NVARCHAR(30) UNIQUE NOT NULL,
     Password NVARCHAR(30) NOT NULL,
-
-    -- там стояло по дефолту 0 оставляем?
     Phone NVARCHAR(30) DEFAULT('0') NOT NULL,
     ImagePath VARCHAR(255) NULL,
 
@@ -38,49 +43,24 @@ CREATE TABLE Users(
 )
 
 
-CREATE TABLE Admins(
-    Id INT IDENTITY(1, 1) NOT NULL,
-    UserId INT NOT NULL UNIQUE,
-    -- foreigh key for userid from users??? and cascade delete if delete ur serlve or what? then delete unique
-    CONSTRAINT PK_Admins PRIMARY KEY CLUSTERED(Id ASC)
-);
-
-
-
-CREATE TABLE Marks(
-    Id INT IDENTITY(1, 1) NOT NULL,
-    UserId INT NOT NULL,
-    -- откуда беретется timetableid по данным это не тоже самое что и timetableid в таблицах есть разные
-    TimeTableId INT NOT NULL,
-    SelectedIndex INT NOT NULL,
-    -- какую дату лучше ставить? стринг или такую
-    Date DATE NOT NULL,
-    -- Cascade delete? лучше наверно да
-    FOREIGN KEY(UserId) REFERENCES Users(Id) ON DELETE SET NULL,
-    CONSTRAINT PK_Marks PRIMARY KEY CLUSTERED(Id ASC)
-);
-
-CREATE TABLE Subjects(
-    Id INT NOT NULL,
-    SubjectName NVARCHAR(50) UNIQUE NOT NULL,
-    CONSTRAINT PK_Subjects PRIMARY KEY CLUSTERED(Id ASC)
-);
-
-
 CREATE TABLE Teachers(
     Id INT IDENTITY(1, 1) NOT NULL,
     UserId INT NOT NULL,
-    SubjectId INT NOT NULL,
-    -- в коде нигде нет класс ментор айди
-    TheClassMentorId INT NOT NULL,
+    SubjectId INT NULL,
+    TheClassMentorId INT NULL,
     FOREIGN KEY(UserId) REFERENCES Users(Id) ON DELETE CASCADE,
-    -- могут быть проблемы если удалим предмет и учитель останется без предмета
+    
     FOREIGN KEY(SubjectId) REFERENCES Subjects(Id) ON DELETE SET NULL,
     CONSTRAINT PK_Teachers PRIMARY KEY CLUSTERED(Id ASC)
 );
 
 
-
+CREATE TABLE Admins(
+    Id INT IDENTITY(1, 1) NOT NULL,
+    UserId INT NULL UNIQUE,
+    FOREIGN KEY(UserId) REFERENCES Users(Id) ON DELETE SET NULL,
+    CONSTRAINT PK_Admins PRIMARY KEY CLUSTERED(Id ASC)
+);
 
 
 CREATE TABLE Timetables(
@@ -91,7 +71,7 @@ CREATE TABLE Timetables(
     TeacherID INT DEFAULT(1) NOT NULL,
     Monday BIT DEFAULT(0) NOT NULL,
     Tuesday BIT DEFAULT(0) NOT NULL,
-    Wednesday BIT DEFAULT(0) NOT NULL,
+    Wednesday BIT DEFAULT(0) NOT NULL, 
     Thursday BIT DEFAULT(0) NOT NULL,
     Friday  BIT DEFAULT(0) NOT NULL,
     FOREIGN KEY(SubjectId) REFERENCES Subjects(Id) ON DELETE CASCADE,
@@ -99,4 +79,16 @@ CREATE TABLE Timetables(
     FOREIGN KEY(TeacherId) REFERENCES Teachers(Id) ON DELETE CASCADE,
 
     CONSTRAINT PK_Timetable PRIMARY KEY CLUSTERED(Id ASC)
+);
+
+
+CREATE TABLE Marks(
+    Id INT IDENTITY(1, 1) NOT NULL,
+    UserId INT NOT NULL,
+    TimeTableId INT NOT NULL,
+    SelectedIndex INT NULL,
+    Date VARCHAR(20) NOT NULL,
+    FOREIGN KEY(TimeTableId) REFERENCES Timetables(Id) ON DELETE CASCADE,
+    FOREIGN KEY(UserId) REFERENCES Users(Id) ON DELETE NO ACTION,
+    CONSTRAINT PK_Marks PRIMARY KEY CLUSTERED(Id ASC)
 );
